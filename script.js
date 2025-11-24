2d771d5ab8f4b59cbf1d7833b8bb7076a6721317
Pankaj Patil <pankajsattadhish@gmail.com>
2025-11-24 15:29:16 +0530

    Write logic to get human choice

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-console.log(Math.trunc(Math.random() * 3));
-// const getComputerChoice = function () {
-//   const choices = ["Rock", "Paper", "Scissor"];
-//   const choiceNum = Math.trunc(Math.random() * 3);
-//   return choices[choiceNum];
-// };
+const getComputerChoice = function () {
+  const choices = ["Rock", "Paper", "Scissor"];
+  const choiceNum = Math.trunc(Math.random() * 3);
+  return choices[choiceNum];
+};
+
+const getHumanChoice = function () {
+  const userChoice = prompt(
+    `Enter your Choice ('rock', 'paper' or 'scissor') :`
+  );
+  return userChoice;
+};
